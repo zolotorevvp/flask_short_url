633acfdceeb23651a11e0f92c306c74715b68007
zolotorevvp <zolotorevvp@gmail.com>
2021-07-05 07:08:10 +0500

    Изменен размер ячеек в БД.

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ db = SQLAlchemy(app)
 
 class ShortURL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(2024), nullable=False)
-    long_url = db.Column(db.String(1024), nullable=False)
+    token = db.Column(db.String(32), nullable=False)
+    long_url = db.Column(db.String(2024), nullable=False)
 
     def __init__(self, token, long_url):
         self.token = token
@@ -25,17 +25,19 @@ db.create_all()
 @app.route('/', methods=['GET', 'POST'])
 def main():
     long_url = request.form.get('URL')
-    try:
-        check_url = requests.get(long_url).status_code
-    except:
-        check_url = 'Неверный URL.'
-    if long_url and check_url == 200:
-        hashids = Hashids(salt=long_url, min_length=6)
-        token = hashids.encode(1)
-        db.session.add(ShortURL(long_url, token))
-        db.session.commit()
-        short_url = ROOT_URL + token
-        return render_template('index.html', short_URL=short_url)
+    check_url = ''
+    if long_url:
+        try:
+            check_url = requests.get(long_url).status_code
+        except:
+            check_url = 'Неверный URL.'
+        if check_url == 200:
+            hashids = Hashids(salt=long_url, min_length=6)
+            token = hashids.encode(1)
+            db.session.add(ShortURL(long_url, token))
+            db.session.commit()
+            short_url = ROOT_URL + token
+            return render_template('index.html', short_URL=short_url)
     return render_template('index.html', short_URL=check_url)
 
 
